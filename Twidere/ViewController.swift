21703b3b173f6310c3b5e1bfec59af28e9486c6e
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-05-27 11:15:31 +0800

    Initial Commit

---
+++ Twidere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twidere
+//
+//  Created by Mariotaku Lee on 16/5/27.
+//  Copyright © 2016年 Mariotaku Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
