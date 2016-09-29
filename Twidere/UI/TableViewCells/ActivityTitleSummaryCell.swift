69aa59aa4b07a99dd1baacafebd1e91ff316fd12
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-09-29 10:24:17 +0800

    adding activity cell

---
+++ Twidere/UI/TableViewCells/ActivityTitleSummaryCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityTitleSummaryCell.swift
+//  Twidere
+//
+//  Created by Mariotaku Lee on 2016/9/29.
+//  Copyright © 2016年 Mariotaku Dev. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTitleSummaryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
