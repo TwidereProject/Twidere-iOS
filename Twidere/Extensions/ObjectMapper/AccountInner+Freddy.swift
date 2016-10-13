e91202b6047077cfaab095b9f7ebf16c1be8d9c3
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-10-13 15:16:43 +0800

    sync

---
+++ Twidere/Extensions/ObjectMapper/AccountInner+Freddy.swift
@@ -0,0 +1,9 @@
+//
+//  AccountInner+Freddy.swift
+//  Twidere
+//
+//  Created by Mariotaku Lee on 2016/10/13.
+//  Copyright © 2016年 Mariotaku Dev. All rights reserved.
+//
+
+import Foundation
---
+++ Twidere/Extensions/ObjectMapper/ActivityInner+Freddy.swift
@@ -0,0 +1,9 @@
+//
+//  ActivityInner+Freddy.swift
+//  Twidere
+//
+//  Created by Mariotaku Lee on 2016/10/13.
+//  Copyright © 2016年 Mariotaku Dev. All rights reserved.
+//
+
+import Foundation
