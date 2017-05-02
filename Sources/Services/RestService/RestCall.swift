6355a3c29e6fec8ce39fd515f3af9efec69a650b
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-05-02 13:42:53 +0800

    removed unused file

---
+++ Sources/Services/RestService/RestCall.swift
@@ -0,0 +1,14 @@
+//
+//  RestCall.swift
+//  Twidere
+//
+//  Created by Mariotaku Lee on 2017/5/2.
+//  Copyright © 2017年 Mariotaku Dev. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class RestCall {
+    var method: HTTPMethod = .get
+}
