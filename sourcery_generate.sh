1e09233088bca6e05250ab4274631c6fccd7c5c5
Mariotaku Lee <mariotaku.lee@gmail.com>
2017-05-04 19:49:03 +0800

    updated file structures

--- sourcery_generate.sh
+++ sourcery_generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for project in 'TwidereCore Twidere'; do
-  ./Pods/Sourcery/bin/sourcery --templates ./Templates/ --sources $project/Sources/ --output $project/Generated
+for project in TwidereCore Twidere; do
+  ./Pods/Sourcery/bin/sourcery --templates Templates/ --sources $project/Sources --output $project/Generated
 done
